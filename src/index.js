bfddd88195392e3d8b9c1333657282e8496349ad
KimDoHyeon <ghkdtprhf5@naver.com>
2024-09-28 22:09:06 +0900

    feat: SSR 지원을 위한 hydrateRoot 적용

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // React 18에서는 'react-dom/client'를 사용
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  //<React.StrictMode>
-    <App />
-  //</React.StrictMode>
+// 서버에서 렌더링된 HTML을 클라이언트에서 hydrateRoot로 재활용
+const rootElement = document.getElementById('root');
+ReactDOM.hydrateRoot(
+  rootElement,
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
