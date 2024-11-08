65065cc54eb8ceb52638fd03be871e427e996e33
KimDoHyeon <ghkdtprhf5@naver.com>
2024-11-08 17:39:46 +0900

    fix: 무한 재렌더링 제거 	- coinData 상태가 실질적인 변화가 있을 때만 업데이트 이를 통해 불필요한 setState 호출 줄임 	- useMemo로 sortedCoinData에 필요한 의존성 값이 실제로 변경될 때만 정렬 및 필터링 작업 수행

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import io from 'socket.io-client';
 import TopArea from './components/topArea/TopArea'; 
 import CoinFilterArea from './components/coinFilterArea/CoinFilterArea';
@@ -6,6 +6,7 @@ import CoinTable from './components/coinTable/CoinTable';
 import ChatApp from './components/chatApp/ChatApp'; 
 
 import updatePremium from './modules/updatePremium';
+
 import './index.css';
 
 function App() {
@@ -13,120 +14,135 @@ function App() {
     const [exchangeRate, setExchangeRate] = useState(null);
     const [selectedCoin, setSelectedCoin] = useState('BTC');
     const [sortedCoinData, setSortedCoinData] = useState([]);
-    const [searchTerm, setSearchTerm] = useState(''); // 검색어 상태 추가
+    const [searchTerm, setSearchTerm] = useState('');
     const [sortConfig, setSortConfig] = useState(() => {
         const savedConfig = localStorage.getItem('sortConfig');
         return savedConfig ? JSON.parse(savedConfig) : { key: 'acc_trade_price_24h', direction: 'desc' };
     });
-    let conntected = null; //웹소켓 연결 여부를 확인하기 위한 변수
+    let connected = null;
 
-  // Socket.io를 통해 데이터를 받아오는 로직
+    // Socket.io를 통해 데이터를 받아오는 로직
     useEffect(() => {
         const socket = io(process.env.REACT_APP_BACKEND_URL);
-
+    
         socket.on('connect', () => {
-            conntected = true;
+            connected = true;
         });
-
+    
         socket.on('initial', (message) => {
             try {
                 setExchangeRate(message.exchangeRate);
-        
                 const { upbit, bybit } = message.data;
                 const formattedData = {};
-        
+    
                 for (const ticker in upbit) {
-                const upbitData = upbit[ticker];
-                const bybitData = bybit[ticker] || { price: null };
-        
-                formattedData[ticker] = {
-                    ticker: ticker,
-                    upbitPrice: upbitData.price,
-                    bybitPrice: bybitData.price,
-                    signedChangeRate: upbitData.signedChangeRate,
-                    lowest_52_week_price: upbitData.lowest_52_week_price,
-                    acc_trade_price_24h: upbitData.acc_trade_price_24h,
-                };
+                    const upbitData = upbit[ticker];
+                    const bybitData = bybit[ticker] || { price: null };
+    
+                    formattedData[ticker] = {
+                        ticker: ticker,
+                        upbitPrice: upbitData.price,
+                        bybitPrice: bybitData.price,
+                        signedChangeRate: upbitData.signedChangeRate,
+                        lowest_52_week_price: upbitData.lowest_52_week_price,
+                        acc_trade_price_24h: upbitData.acc_trade_price_24h,
+                    };
                 }
-        
-                setCoinData(formattedData); // 초기 데이터는 비교 없이 바로 설정
+    
+                setCoinData((prevData) => {
+                    // 데이터가 변경된 경우에만 업데이트
+                    if (JSON.stringify(prevData) !== JSON.stringify(formattedData)) {
+                        return formattedData;
+                    }
+                    return prevData;
+                });
             } catch (error) {
                 console.error("Error parsing initial data:", error);
             }
         });
-
+    
         socket.on('upbit', (message) => {
             const { ticker, price, signedChangeRate, acc_trade_price_24h } = message;
-        
+    
             setCoinData((prevData) => {
                 const updatedData = { ...prevData };
-
+    
                 if (!updatedData[ticker]) {
-                updatedData[ticker] = { upbitPrice: null, bybitPrice: null, signedChangeRate: null, acc_trade_price_24h: null };
+                    updatedData[ticker] = { upbitPrice: null, bybitPrice: null, signedChangeRate: null, acc_trade_price_24h: null };
                 }
-
-                updatedData[ticker].upbitPrice = price;
-                updatedData[ticker].signedChangeRate = signedChangeRate;
-                updatedData[ticker].acc_trade_price_24h = acc_trade_price_24h;
-        
-                return updatedData;
+    
+                // 데이터 변경이 있을 때만 업데이트
+                if (
+                    updatedData[ticker].upbitPrice !== price ||
+                    updatedData[ticker].signedChangeRate !== signedChangeRate ||
+                    updatedData[ticker].acc_trade_price_24h !== acc_trade_price_24h
+                ) {
+                    updatedData[ticker].upbitPrice = price;
+                    updatedData[ticker].signedChangeRate = signedChangeRate;
+                    updatedData[ticker].acc_trade_price_24h = acc_trade_price_24h;
+                    return updatedData;
+                }
+                return prevData;
             });
         });
-
+    
         socket.on('bybit', (message) => {
             const { ticker, price } = message;
-        
+    
             setCoinData((prevData) => {
                 const updatedData = { ...prevData };
-
+    
                 if (!updatedData[ticker]) {
-                updatedData[ticker] = { upbitPrice: null, bybitPrice: null, signedChangeRate: null, acc_trade_price_24h: null };
+                    updatedData[ticker] = { upbitPrice: null, bybitPrice: null, signedChangeRate: null, acc_trade_price_24h: null };
                 }
-
-                updatedData[ticker].bybitPrice = price;
-        
-                return updatedData;
+    
+                // 데이터 변경이 있을 때만 업데이트
+                if (updatedData[ticker].bybitPrice !== price) {
+                    updatedData[ticker].bybitPrice = price;
+                    return updatedData;
+                }
+                return prevData;
             });
         });
-
+    
         socket.on('exchangeRateUpdate', (message) => {
-            setExchangeRate(message.exchangeRate);
+            setExchangeRate((prevRate) => {
+                // exchangeRate에 변화가 있을 때만 업데이트
+                return prevRate !== message.exchangeRate ? message.exchangeRate : prevRate;
+            });
         });
-
+    
         socket.on('disconnect', () => {
-        conntected = false;
+            connected = false;
         });
-
+    
         return () => {
-        socket.disconnect();
+            socket.disconnect();
         };
     }, []);
+    
 
     // 코인 데이터를 정렬하는 로직
-    useEffect(() => {
-        const sortedData = Object.keys(coinData)
-        .map(ticker => ({ ticker, ...coinData[ticker] }))
-        .filter(coin => coin.ticker.toLowerCase().includes(searchTerm.toLowerCase())) // 검색어로 필터링
-        .sort((a, b) => {
-            const { key, direction } = sortConfig;
+    const sortedData = useMemo(() => {
+        return Object.keys(coinData)
+            .map(ticker => ({ ticker, ...coinData[ticker] }))
+            .filter(coin => coin.ticker.toLowerCase().includes(searchTerm.toLowerCase()))
+            .sort((a, b) => {
+                const { key, direction } = sortConfig;
 
-            // 김치 프리미엄 백분율에 대한 정렬을 추가
-            if (key === 'premiumValue') {
-            const aPremium = updatePremium(a.ticker, a, exchangeRate).premiumRate;
-            const bPremium = updatePremium(b.ticker, b, exchangeRate).premiumRate;
-            return direction === 'asc' ? aPremium - bPremium : bPremium - aPremium;
-            }
+                if (key === 'premiumValue') {
+                    const aPremium = updatePremium(a.ticker, a, exchangeRate).premiumRate;
+                    const bPremium = updatePremium(b.ticker, b, exchangeRate).premiumRate;
+                    return direction === 'asc' ? aPremium - bPremium : bPremium - aPremium;
+                }
 
-            // 다른 필드에 대한 기본 정렬
-            if (direction === 'asc') {
-            return a[key] > b[key] ? 1 : -1;
-            } else {
-            return a[key] < b[key] ? 1 : -1;
-            }
-        });
-
-        setSortedCoinData(sortedData);
+                return direction === 'asc' ? (a[key] > b[key] ? 1 : -1) : (a[key] < b[key] ? 1 : -1);
+            });
     }, [coinData, sortConfig, searchTerm, exchangeRate]);
+
+    useEffect(() => {
+        setSortedCoinData(sortedData);
+    }, [sortedData]);
 
     useEffect(() => {
         localStorage.setItem('sortConfig', JSON.stringify(sortConfig));
@@ -138,42 +154,42 @@ function App() {
 
     const handleSort = (key) => {
         setSortConfig((prevConfig) => ({
-        key,
-        direction: prevConfig.direction === 'asc' ? 'desc' : 'asc',
+            key,
+            direction: prevConfig.direction === 'asc' ? 'desc' : 'asc',
         }));
     };
 
     const handleSearch = (term) => {
-        setSearchTerm(term); // 검색어 상태 업데이트
+        setSearchTerm(term);
     };
 
     return (
         <div className='mainContainer'>
-        <div className='topArea'>
-            <TopArea exchangeRate = {exchangeRate}/>
-        </div>
-        <div className='mainContent'>
-            <div className='container width1200 width990 width770 widthother'>
-            <div className='coinInfo'>
-                <CoinFilterArea 
-                coin={selectedCoin} 
-                data={coinData[selectedCoin]} 
-                exchangeRate={exchangeRate}
-                onSearch={handleSearch} // 검색 기능을 위한 props 전달
-                />
-                <CoinTable 
-                coinData={sortedCoinData} 
-                exchangeRate={exchangeRate} 
-                onCoinClick={handleCoinClick} 
-                onSort={handleSort} 
-                sortConfig={sortConfig} 
-                />
+            <div className='topArea'>
+                <TopArea exchangeRate={exchangeRate} />
             </div>
-            <div className='chatApp'>
-                <ChatApp />
+            <div className='mainContent'>
+                <div className='container width1200 width990 width770 widthother'>
+                    <div className='coinInfo'>
+                        <CoinFilterArea 
+                            coin={selectedCoin} 
+                            data={coinData[selectedCoin]} 
+                            exchangeRate={exchangeRate}
+                            onSearch={handleSearch}
+                        />
+                        <CoinTable 
+                            coinData={sortedCoinData} 
+                            exchangeRate={exchangeRate} 
+                            onCoinClick={handleCoinClick} 
+                            onSort={handleSort} 
+                            sortConfig={sortConfig} 
+                        />
+                    </div>
+                    <div className='chatApp'>
+                        <ChatApp />
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
         </div>
     );
 }
