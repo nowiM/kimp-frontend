04d7144f85d3a188d1687fa21f8e0bf8e9f55d80
KimDoHyeon <ghkdtprhf5@naver.com>
2025-01-07 16:46:05 +0900

    Refactor: 클라이언트측 Socket.io 처리 로직 분리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState, useMemo } from 'react';
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 import TopArea from './components/topArea/TopArea'; 
 import CoinFilterArea from './components/coinFilterArea/CoinFilterArea';
 import CoinTable from './components/coinTable/CoinTable';
 import ChatApp from './components/chatApp/ChatApp'; 
 
+import createWebsocket from './utils/createWebsoket';
 import updatePremium from './modules/updatePremium';
 
 import './index.css';
@@ -23,94 +24,8 @@ function App() {
 
     // WebSocket 연결 로직
     useEffect(() => {
-        const socket = io(process.env.REACT_APP_BACKEND_URL);
-
-        socket.on('connect', () => {
-            console.log('[WebSocket] Connected');
-        });
-
-        socket.on('initial', (message) => {
-            try {
-                setExchangeRate(message.exchangeRate);
-                const { upbit, bybit } = message.data;
-                const formattedData = {};
-    
-                for (const ticker in upbit) {
-                    const upbitData = upbit[ticker];
-                    const bybitData = bybit[ticker] || { price: null };
-    
-                    formattedData[ticker] = {
-                        ticker: ticker,
-                        upbitPrice: upbitData.price,
-                        bybitPrice: bybitData.price,
-                        signedChangeRate: upbitData.signedChangeRate,
-                        lowest_52_week_price: upbitData.lowest_52_week_price,
-                        acc_trade_price_24h: upbitData.acc_trade_price_24h,
-                    };
-                }
-    
-                setCoinData((prevData) => {
-                    if (JSON.stringify(prevData) !== JSON.stringify(formattedData)) {
-                        return formattedData;
-                    }
-                    return prevData;
-                });
-            } catch (error) {
-                console.error("Error parsing initial data:", error);
-            }
-        });
-
-        socket.on('upbit', (message) => {
-            const { ticker, price, signedChangeRate, acc_trade_price_24h } = message;
-            setCoinData((prevData) => {
-                const updatedData = { ...prevData };
-    
-                if (!updatedData[ticker]) {
-                    updatedData[ticker] = { upbitPrice: null, bybitPrice: null, signedChangeRate: null, acc_trade_price_24h: null };
-                }
-    
-                if (
-                    updatedData[ticker].upbitPrice !== price ||
-                    updatedData[ticker].signedChangeRate !== signedChangeRate ||
-                    updatedData[ticker].acc_trade_price_24h !== acc_trade_price_24h
-                ) {
-                    updatedData[ticker].upbitPrice = price;
-                    updatedData[ticker].signedChangeRate = signedChangeRate;
-                    updatedData[ticker].acc_trade_price_24h = acc_trade_price_24h;
-                    return updatedData;
-                }
-                return prevData;
-            });
-        });
-
-        socket.on('bybit', (message) => {
-            const { ticker, price } = message;
-            setCoinData((prevData) => {
-                const updatedData = { ...prevData };
-    
-                if (!updatedData[ticker]) {
-                    updatedData[ticker] = { upbitPrice: null, bybitPrice: null, signedChangeRate: null, acc_trade_price_24h: null };
-                }
-    
-                if (updatedData[ticker].bybitPrice !== price) {
-                    updatedData[ticker].bybitPrice = price;
-                    return updatedData;
-                }
-                return prevData;
-            });
-        });
-
-        socket.on('exchangeRateUpdate', (message) => {
-            setExchangeRate((prevRate) => {
-                return prevRate !== message.exchangeRate ? message.exchangeRate : prevRate;
-            });
-        });
-
-        socket.on('disconnect', () => {
-            console.warn('[WebSocket] Disconnected. Reconnecting...');
-            socket.connect(); // 단순 재연결
-        });
-
+        const socket = createWebsocket(process.env.REACT_APP_BACKEND_URL, { setCoinData, setExchangeRate });
+        
         return () => {
             socket.disconnect();
         };
