de641ef77ebb76a57dd875c508bf0c5ed7fd2f14
KimDoHyeon <ghkdtprhf5@naver.com>
2025-02-06 10:17:36 +0900

    feat: 이미지 최적화 배포서버 테스트

--- src/components/coinTable/CoinRow.js
+++ src/components/coinTable/CoinRow.js
@@ -36,6 +36,7 @@ const CoinRow = ({ ticker, data, exchangeRate, onClick, onBookmarkToggle }) => {
     }
   }, [data.upbitPrice, previousUpbitPrice]);
   
+  // 배포 서버 테스트
   return (
     <tr id={`coin-${ticker}`} onClick={onClick} style={{ cursor: 'pointer' }}>
       <td className='simbolInfoTd'>
